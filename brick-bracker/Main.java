8d9deba18d5f75e146034b3c352c7b372421fc75
TallaVa <behram89@gmail.com>
2021-02-12 10:18:16 +0100

    på G

--- brick-bracker/Main.java
+++ brick-bracker/Main.java
@@ -5,11 +5,13 @@ public class Main
     public static void main(String[] args)
     {
         JFrame obj = new JFrame();
+        Gameplay gamePlay = new Gameplay();
         obj.setBounds(10, 10, 700, 600);
         obj.setTitle("Brick Breaker XL");
         obj.setResizable(false);
         obj.setVisible(true);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
         
     }
 
